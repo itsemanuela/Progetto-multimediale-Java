9c1914149d5b5346da46e9e3ea13ea3ceac832ca
itsemanuela <notemanu152@gmail.com>
2026-06-07 11:39:45 +0200

    Class Video e test Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import entities.Multimedia;
 import java.util.Scanner;
 import entities.Audio;
+import entities.Video;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -18,5 +20,39 @@ public class Main {
 
         //Funziona correttamente percè ho stampato il titolo per 3 volte che sarebbe la durata, mi stampa 5 punti escalamtivi perchè ho incrementato di 1 nella class Audio.
 
+        //test video come per audio, stesso ragionamento. Ho importato nel main il package entities.Video e gli ho passato semplicemente i parametri coi miei valori. Come si può notare dall'esecuzione ottengo il risultato desiderato.
+        Video mioVideo= new Video("Video", 6, 5, 1);
+        mioVideo.alzaVolume();
+        mioVideo.aumentaLuminosita();
+        mioVideo.play();
+
+        //Testo anche la funzionalità dello scanner
+        System.out.println("Inserisci il NUMERO del titolo del film che vuoi riprodurre da 1 a 5 ");
+        int titoloFilm=scanner.nextInt();
+
+        switch(titoloFilm){
+            case 1:
+                System.out.println("Harry Potter");
+                break;
+                case 2:
+                    System.out.println("Il Signore degli Anelli");
+                    break;
+                    case 3:
+                        System.out.println("Spider-Man");
+                        break;
+            case 4:
+                System.out.println("Le Cronache di Narnia");
+                break;
+                case 5:
+                    System.out.println("BatMan");
+                    break;
+                    default:
+                        System.out.println("Vuoi riprovare?");
+                        System.out.println("Consigliaci un film da inserire: ");
+                        String titoloInesistente= scanner.nextLine();
+                        String filmConsigliato = scanner.nextLine();
+                        System.out.println("Grazie! Prenderemo in considerazione " + filmConsigliato);
+                        break;
+        }
     }
 }
--- src/entities/Video.java
+++ src/entities/Video.java
@@ -1,4 +1,60 @@
 package entities;
+import Interfaces.Riproducibile;
 
-public class Video {
+// Anche Video, come Audio estende Multimedia (per il titolo) e implementa Riproducibile (per il play) che però andrò a sovrascrivere in quanto qui ho un ulteriore parametro.
+public class Video extends Multimedia implements Riproducibile {
+    private int durata;
+    private int volume;
+    private int luminosita;
+
+    //metto questi "dati sensibili" private per la "legge dell'incapsulamento" (sicurezza).
+
+    // COSTRUTTORE: passo 4 parametri come per Audio
+    public Video(String titolo, int durata, int volumeIniziale, int luminositaIniziale) {
+        // anche qui prendo titolo e lo mando a multimedia
+        super(titolo);
+        this.durata = durata;
+        this.volume = volumeIniziale;
+        this.luminosita = luminositaIniziale;
+    }
+
+    // stessa cosa fatta per audio
+    public void alzaVolume() {
+        volume = volume + 1;
+    }
+
+    public void abbassaVolume() {
+        if (volume > 0) {
+            volume = volume - 1;
+        }
+    }
+
+    //  gestione Luminosità solo per video al momento
+    public void aumentaLuminosita() {
+        luminosita = luminosita + 1;
+    }
+
+    public void diminuisciLuminosita() {
+        if (luminosita > 0) {
+            luminosita = luminosita - 1;
+        }
+    }
+
+    // Il metodo play() del Video deve stampare sia il volume (!) che la luminosità (*), per cui riscrivo il metodo play personalizzandolo
+    @Override
+    public void play() {
+        for (int i = 0; i < durata; i++) {
+            System.out.print(titolo + " ");
+            for (int v = 0; v < volume; v++) {
+                System.out.print("!");
+            }
+
+            // ciclo for per la luminosità
+            for (int l = 0; l < luminosita; l++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
 }
