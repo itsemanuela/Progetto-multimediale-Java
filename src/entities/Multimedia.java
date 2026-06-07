91223433f8a6e0fe186806b6bbb96d652e3f6f27
itsemanuela <notemanu152@gmail.com>
2026-06-07 10:32:22 +0200

    Abstract multimedia

--- src/entities/Multimedia.java
+++ src/entities/Multimedia.java
@@ -1,4 +1,19 @@
 package entities;
 
-public class Multimedia {
+// Parto dalla classe astratta base Multimedia. Contiene l'attributo titolo che è comune a tutti gli elementi (audio, video e immagini) e il costruttore. È astratta perché non ha senso creare un oggetto 'multimedia' generico; serve solo come modello per le classi specifiche. Rappresenta solo l'idea generale di un file multimediale //
+
+
+
+public abstract class Multimedia {
+    protected String titolo;
+
+    // Costruttore
+    public Multimedia(String titolo) {
+        this.titolo = titolo;
+    }
+
+    // Metodo getter per leggere il titolo
+    public String getTitolo() {
+        return titolo;
+    }
 }
